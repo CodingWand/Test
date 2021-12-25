f53190bf1794811086ef5985c8adf5a5f0fbad24
assane <assane.diouf@centrale-marseille.com>
2021-12-25 10:37:22 +0100

    Création de la db et de User et Equipment

---
+++ src/Entity/Equipment.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EquipmentRepository::class)
+ */
+class Equipment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantify;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $availableStock;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $allowedDays;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $uid;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getQuantify(): ?int
+    {
+        return $this->quantify;
+    }
+
+    public function setQuantify(int $quantify): self
+    {
+        $this->quantify = $quantify;
+
+        return $this;
+    }
+
+    public function getAvailableStock(): ?int
+    {
+        return $this->availableStock;
+    }
+
+    public function setAvailableStock(int $availableStock): self
+    {
+        $this->availableStock = $availableStock;
+
+        return $this;
+    }
+
+    public function getAllowedDays(): ?int
+    {
+        return $this->allowedDays;
+    }
+
+    public function setAllowedDays(int $allowedDays): self
+    {
+        $this->allowedDays = $allowedDays;
+
+        return $this;
+    }
+
+    public function getUid(): ?string
+    {
+        return $this->uid;
+    }
+
+    public function setUid(string $uid): self
+    {
+        $this->uid = $uid;
+
+        return $this;
+    }
+}
